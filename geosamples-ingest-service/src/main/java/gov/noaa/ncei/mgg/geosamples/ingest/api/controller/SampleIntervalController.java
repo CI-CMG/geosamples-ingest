d11e055ae9b559bc18a0592d40f82aad33875dd3
paytoncain <payton.cain@colorado.edu>
2023-02-01 16:19:29 -0700

    Add export endpoint. Add excel workbook writer

--- geosamples-ingest-service/src/main/java/gov/noaa/ncei/mgg/geosamples/ingest/api/controller/SampleIntervalController.java
+++ geosamples-ingest-service/src/main/java/gov/noaa/ncei/mgg/geosamples/ingest/api/controller/SampleIntervalController.java
@@ -1,11 +1,22 @@
 package gov.noaa.ncei.mgg.geosamples.ingest.api.controller;
 
+import gov.noaa.ncei.mgg.geosamples.ingest.api.error.ApiError;
+import gov.noaa.ncei.mgg.geosamples.ingest.api.error.ApiException;
 import gov.noaa.ncei.mgg.geosamples.ingest.api.model.CombinedIntervalSampleSearchParameters;
 import gov.noaa.ncei.mgg.geosamples.ingest.api.model.CombinedSampleIntervalView;
 import gov.noaa.ncei.mgg.geosamples.ingest.api.model.SimpleItemsView;
 import gov.noaa.ncei.mgg.geosamples.ingest.api.model.paging.PagedItemsView;
+import gov.noaa.ncei.mgg.geosamples.ingest.service.ExcelWorkbookWriter;
 import gov.noaa.ncei.mgg.geosamples.ingest.service.SampleIntervalService;
+import gov.noaa.ncei.mgg.geosamples.ingest.service.model.SampleRow;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -31,6 +42,99 @@ public class SampleIntervalController {
   @PatchMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
   public SimpleItemsView<CombinedSampleIntervalView> accept(@Valid @RequestBody SimpleItemsView<CombinedSampleIntervalView> patch) {
     return sampleIntervalService.patch(patch);
+  }
+
+  @GetMapping(path = "/export")
+  public void export(HttpServletResponse response, @Valid CombinedIntervalSampleSearchParameters searchParameters) {
+    searchParameters.setItemsPerPage(1000);
+
+    PagedItemsView<CombinedSampleIntervalView> page = sampleIntervalService.search(searchParameters);
+    if (page.getTotalItems() == 0) {
+      throw new ApiException(
+          HttpStatus.BAD_REQUEST,
+          ApiError.builder().error("Query returned no results to export").build()
+      );
+    }
+    response.setContentType(new MediaType("application", "vnd.ms-excel").toString());
+    response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=export.xlsx");
+    try (
+        OutputStream outputStream = response.getOutputStream();
+        ExcelWorkbookWriter excelWorkBookWriter = new ExcelWorkbookWriter("Data Output", outputStream)
+    ) {
+      while (page.getPage() <= page.getTotalPages()) {
+        excelWorkBookWriter.writeToSheet(
+            page.getItems().stream()
+                .map(SampleIntervalController::toSampleRow)
+                .collect(Collectors.toList())
+        );
+
+        searchParameters.setPage(searchParameters.getPage() + 1);
+        page = sampleIntervalService.search(searchParameters);
+      }
+      outputStream.flush();
+    } catch (IOException e) {
+      throw new IllegalStateException("Failed to export to file");
+    }
+  }
+
+  private static SampleRow toSampleRow(CombinedSampleIntervalView view) {
+    SampleRow sampleRow = new SampleRow();
+    sampleRow.setFacilityCode(view.getFacility());
+    sampleRow.setShipName(view.getPlatform());
+    sampleRow.setCruiseId(view.getCruise());
+    sampleRow.setSampleId(view.getSample());
+    sampleRow.setDateCollected(view.getBeginDate());
+    sampleRow.setEndDate(view.getEndDate());
+    sampleRow.setBeginningLatitude(view.getLat());
+    sampleRow.setBeginningLongitude(view.getLon());
+    sampleRow.setEndingLatitude(view.getEndLat());
+    sampleRow.setEndingLongitude(view.getEndLon());
+    if (view.getWaterDepth() != null) {
+      sampleRow.setBeginningWaterDepth(Double.valueOf(view.getWaterDepth()));
+    }
+    if (view.getEndWaterDepth() != null) {
+      sampleRow.setEndingWaterDepth(Double.valueOf(view.getEndWaterDepth()));
+    }
+    sampleRow.setSamplingDeviceCode(view.getDevice());
+    sampleRow.setStorageMethodCode(view.getStorageMeth());
+    if (view.getCoredLength() != null) {
+      sampleRow.setCoreLength(Double.valueOf(view.getCoredLength()));
+    }
+    if (view.getCoredDiam() != null) {
+      sampleRow.setCoreDiameter(Double.valueOf(view.getCoredDiam()));
+    }
+    if (view.getDepthTop() != null) {
+      sampleRow.setDepthToTopOfInterval(Double.valueOf(view.getDepthTop()));
+    }
+    if (view.getDepthBot() != null) {
+      sampleRow.setDepthToBottomOfInterval(Double.valueOf(view.getDepthBot()));
+    }
+    sampleRow.setPrimaryLithologicCompositionCode(view.getLith1());
+    sampleRow.setPrimaryTextureCode(view.getText1());
+    sampleRow.setSecondaryLithologicCompositionCode(view.getLith2());
+    sampleRow.setSecondaryTextureCode(view.getText2());
+    sampleRow.setOtherComponentCodes(
+        Stream.of(
+        view.getComp1(), view.getComp2(), view.getComp3(), view.getComp4(), view.getComp5(), view.getComp6()
+        ).filter(c -> c != null && !c.isEmpty())
+            .collect(Collectors.toList())
+    );
+    sampleRow.setGeologicAgeCode(view.getAge());
+    sampleRow.setIntervalNumber(view.getInterval());
+    sampleRow.setBulkWeight(view.getWeight());
+    sampleRow.setPhysiographicProvinceCode(view.getProvince());
+    sampleRow.setSampleLithologyCode(view.getRockLith());
+    sampleRow.setSampleMineralogyCode(view.getRockMin());
+    sampleRow.setSampleWeatheringOrMetamorphismCode(view.getWeathMeta());
+    sampleRow.setGlassRemarksCode(view.getRemark());
+    sampleRow.setMunsellColor(view.getMunsellCode());
+    sampleRow.setPrincipalInvestigator(view.getPi());
+    sampleRow.setSampleNotAvailable(view.getExhaustCode());
+    sampleRow.setIgsn(view.getIgsn());
+    sampleRow.setAlternateCruise(view.getLeg());
+    sampleRow.setDescription(view.getDescription());
+    sampleRow.setComments(view.getIntComments());
+    return sampleRow;
   }
 
 }
---
+++ geosamples-ingest-service/src/main/java/gov/noaa/ncei/mgg/geosamples/ingest/service/ExcelWorkbookWriter.java
@@ -0,0 +1,180 @@
+package gov.noaa.ncei.mgg.geosamples.ingest.service;
+
+import gov.noaa.ncei.mgg.geosamples.ingest.service.model.SampleRow;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelWorkbookWriter implements Closeable {
+
+  private final Sheet sheet;
+  private final Workbook workbook;
+  private final OutputStream outputStream;
+  private int rowNum = 1;
+  private int nOtherComponents = 0;
+
+  public ExcelWorkbookWriter(String sheetName, OutputStream outputStream) {
+    this.outputStream = outputStream;
+    this.workbook = new XSSFWorkbook();
+    this.sheet = this.workbook.createSheet(sheetName);
+  }
+
+  public void writeToSheet(List<SampleRow> sampleRows) throws IOException {
+    Row row;
+
+    for (SampleRow sampleRow : sampleRows) {
+      row = sheet.createRow(rowNum);
+      if (sampleRow.getFacilityCode() != null) {
+       row.createCell(0).setCellValue(sampleRow.getFacilityCode());
+      }
+      if (sampleRow.getShipName() != null) {
+       row.createCell(1).setCellValue(sampleRow.getShipName());
+      }
+      if (sampleRow.getCruiseId() != null) {
+       row.createCell(2).setCellValue(sampleRow.getCruiseId());
+      }
+      if (sampleRow.getSampleId() != null) {
+       row.createCell(3).setCellValue(sampleRow.getSampleId());
+      }
+      if (sampleRow.getDateCollected() != null) {
+       row.createCell(4).setCellValue(sampleRow.getDateCollected());
+      }
+      if (sampleRow.getEndDate() != null) {
+       row.createCell(5).setCellValue(sampleRow.getEndDate());
+      }
+      if (sampleRow.getBeginningLatitude() != null) {
+       row.createCell(6).setCellValue(sampleRow.getBeginningLatitude());
+      }
+      if (sampleRow.getBeginningLongitude() != null) {
+       row.createCell(7).setCellValue(sampleRow.getBeginningLongitude());
+      }
+      if (sampleRow.getEndingLatitude() != null) {
+       row.createCell(8).setCellValue(sampleRow.getEndingLatitude());
+      }
+      if (sampleRow.getEndingLongitude() != null) {
+       row.createCell(9).setCellValue(sampleRow.getEndingLongitude());
+      }
+      if (sampleRow.getBeginningWaterDepth() != null) {
+       row.createCell(10).setCellValue(sampleRow.getBeginningWaterDepth());
+      }
+      if (sampleRow.getEndingWaterDepth() != null) {
+       row.createCell(11).setCellValue(sampleRow.getEndingWaterDepth());
+      }
+      if (sampleRow.getSamplingDeviceCode() != null) {
+       row.createCell(12).setCellValue(sampleRow.getSamplingDeviceCode());
+      }
+      if (sampleRow.getStorageMethodCode() != null) {
+       row.createCell(13).setCellValue(sampleRow.getStorageMethodCode());
+      }
+      if (sampleRow.getCoreLength() != null) {
+       row.createCell(14).setCellValue(sampleRow.getCoreLength());
+      }
+      if (sampleRow.getCoreDiameter() != null) {
+       row.createCell(15).setCellValue(sampleRow.getCoreDiameter());
+      }
+      if (sampleRow.getDepthToTopOfInterval() != null) {
+       row.createCell(16).setCellValue(sampleRow.getDepthToTopOfInterval());
+      }
+      if (sampleRow.getDepthToBottomOfInterval() != null) {
+       row.createCell(17).setCellValue(sampleRow.getDepthToBottomOfInterval());
+      }
+      if (sampleRow.getPrimaryLithologicCompositionCode() != null) {
+       row.createCell(18).setCellValue(sampleRow.getPrimaryLithologicCompositionCode());
+      }
+      if (sampleRow.getPrimaryTextureCode() != null) {
+       row.createCell(19).setCellValue(sampleRow.getPrimaryTextureCode());
+      }
+      if (sampleRow.getSecondaryLithologicCompositionCode() != null) {
+       row.createCell(20).setCellValue(sampleRow.getSecondaryLithologicCompositionCode());
+      }
+      if (sampleRow.getSecondaryTextureCode() != null) {
+       row.createCell(21).setCellValue(sampleRow.getSecondaryTextureCode());
+      }
+      if (sampleRow.getGeologicAgeCode() != null) {
+       row.createCell(22).setCellValue(sampleRow.getGeologicAgeCode());
+      }
+      if (sampleRow.getIntervalNumber() != null) {
+       row.createCell(23).setCellValue(sampleRow.getIntervalNumber());
+      }
+      if (sampleRow.getBulkWeight() != null) {
+       row.createCell(24).setCellValue(sampleRow.getBulkWeight());
+      }
+      if (sampleRow.getPhysiographicProvinceCode() != null) {
+       row.createCell(25).setCellValue(sampleRow.getPhysiographicProvinceCode());
+      }
+      if (sampleRow.getSampleLithologyCode() != null) {
+       row.createCell(26).setCellValue(sampleRow.getSampleLithologyCode());
+      }
+      if (sampleRow.getSampleMineralogyCode() != null) {
+       row.createCell(27).setCellValue(sampleRow.getSampleMineralogyCode());
+      }
+      if (sampleRow.getSampleWeatheringOrMetamorphismCode() != null) {
+       row.createCell(28).setCellValue(sampleRow.getSampleWeatheringOrMetamorphismCode());
+      }
+      if (sampleRow.getGlassRemarksCode() != null) {
+       row.createCell(29).setCellValue(sampleRow.getGlassRemarksCode());
+      }
+      if (sampleRow.getMunsellColor() != null) {
+       row.createCell(30).setCellValue(sampleRow.getMunsellColor());
+      }
+      if (sampleRow.getPrincipalInvestigator() != null) {
+       row.createCell(31).setCellValue(sampleRow.getPrincipalInvestigator());
+      }
+      if (sampleRow.getSampleNotAvailable() != null) {
+       row.createCell(32).setCellValue(sampleRow.getSampleNotAvailable());
+      }
+      if (sampleRow.getIgsn() != null) {
+       row.createCell(33).setCellValue(sampleRow.getIgsn());
+      }
+      if (sampleRow.getAlternateCruise() != null) {
+       row.createCell(34).setCellValue(sampleRow.getAlternateCruise());
+      }
+      if (sampleRow.getDescription() != null) {
+       row.createCell(35).setCellValue(sampleRow.getDescription());
+      }
+      if (sampleRow.getComments() != null) {
+       row.createCell(36).setCellValue(sampleRow.getComments());
+      }
+
+      int cellNum = 37;
+      for (String componentCode : sampleRow.getOtherComponentCodes()) {
+        row.createCell(cellNum).setCellValue(componentCode);
+        cellNum += 1;
+      }
+      int currentNOtherComponents = cellNum - 37;
+      if (currentNOtherComponents > nOtherComponents) {
+        nOtherComponents = currentNOtherComponents;
+      }
+
+      rowNum += 1;
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    Row row = sheet.createRow(0);
+    int cellNum = 0;
+    for (HeaderNames headerName : HeaderNames.values()) {
+      if (!headerName.equals(HeaderNames.OTHER_COMPONENT_CODE)) {
+        row.createCell(cellNum).setCellValue(headerName.getText());
+        cellNum += 1;
+      }
+    }
+    if (nOtherComponents > 1) {
+      for (int i = 0; i < nOtherComponents; i++) {
+        row.createCell(cellNum).setCellValue(String.format("%s (%s)", HeaderNames.OTHER_COMPONENT_CODE.getText(), i + 1));
+        cellNum += 1;
+      }
+    } else {
+      row.createCell(cellNum).setCellValue(HeaderNames.OTHER_COMPONENT_CODE.getText());
+    }
+    workbook.write(outputStream);
+
+    workbook.close();
+  }
+}

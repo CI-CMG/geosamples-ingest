c8f97700e0bcaa8bba1b50a220c84965fb807e02
paytoncain <payton.cain@colorado.edu>
2023-02-01 21:24:02 -0700

    Add version to Home welcome message

--- geosamples-ingest-ui/src/main/frontend/src/resourceBasePath.js
+++ geosamples-ingest-ui/src/main/frontend/src/resourceBasePath.js
@@ -4,3 +4,4 @@ __webpack_public_path__ = window.applicationConfiguration.contextRoot.trim();
 export const RAW_BASE_PATH = __webpack_public_path__;
 export const AUTH_SERVICE_URL = window.applicationConfiguration.authServiceUrl;
 export const CLIENT_ID = window.applicationConfiguration.clientId;
+export const VERSION = window.applicationConfiguration.version;

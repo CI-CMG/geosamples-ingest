594a98987da53f490cf7b3289ecd00e4c9153195
paytoncain <payton.cain@colorado.edu>
2023-05-09 12:17:15 -0600

    Add base approval controller

---
+++ geosamples-ingest-service/src/main/java/gov/noaa/ncei/mgg/geosamples/ingest/api/controller/ApprovalControllerBase.java
@@ -0,0 +1,27 @@
+package gov.noaa.ncei.mgg.geosamples.ingest.api.controller;
+
+import gov.noaa.ncei.mgg.geosamples.ingest.api.model.ApprovalView;
+import gov.noaa.ncei.mgg.geosamples.ingest.api.model.paging.PagingAndSortingParameters;
+import gov.noaa.ncei.mgg.geosamples.ingest.service.ApprovalResourceServiceBase;
+import javax.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Validated
+public class ApprovalControllerBase<V, S extends PagingAndSortingParameters, I, T extends ApprovalResourceServiceBase<I, ?, S, V, ?>> extends ControllerBase<V, S, I, T> {
+
+  protected final T service;
+
+  public ApprovalControllerBase(T service) {
+    super(service);
+    this.service = service;
+  }
+
+  @PatchMapping(path = "/review/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public V review(@PathVariable I id, @Valid @RequestBody ApprovalView approval) {
+    return service.updateApproval(approval, id);
+  }
+}

249035ae1f750462591c95e8fe19a82c11fb2ede
paytoncain <payton.cain@colorado.edu>
2023-05-24 09:28:37 -0600

    Change column order for provider cruise table

--- geosamples-ingest-ui/src/main/frontend/src/views/view/main/provider/cruise/list/ProviderCruiseList.vue
+++ geosamples-ingest-ui/src/main/frontend/src/views/view/main/provider/cruise/list/ProviderCruiseList.vue
@@ -41,6 +41,11 @@ export default {
     return {
       tableFields: [
         {
+          key: 'approvalState',
+          label: 'Approval State',
+          sortable: false,
+        },
+        {
           key: 'cruiseName',
           label: 'Cruise Name',
           sortable: true,
@@ -59,11 +64,6 @@ export default {
           key: 'platforms',
           label: 'Platforms',
           sortable: true,
-        },
-        {
-          key: 'approvalState',
-          label: 'Approval State',
-          sortable: false,
         },
       ],
     };

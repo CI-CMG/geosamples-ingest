031ba078361ee73e4c08701a48f1818b4ef99dd5
paytoncain <payton.cain@colorado.edu>
2023-05-26 12:33:00 -0600

    Update references to "Interval" in ProviderIntervalList

--- geosamples-ingest-ui/src/main/frontend/src/views/view/main/provider/interval/list/ProviderIntervalList.vue
+++ geosamples-ingest-ui/src/main/frontend/src/views/view/main/provider/interval/list/ProviderIntervalList.vue
@@ -9,14 +9,14 @@
     :fields="[]"
     :table-fields="tableFields"
     create-route="ProviderIntervalAdd"
-    create-text="Add New Interval"
+    create-text="Add New Subsample/Interval"
     create-authority="ROLE_PROVIDER_INTERVAL_CREATE"
     edit-field="interval"
     edit-authority="ROLE_PROVIDER_INTERVAL_UPDATE"
     edit-parameter="id"
     edit-route="ProviderIntervalEdit"
     :is-approval="true"
-    approval-text="Interval Review"
+    approval-text="Subsample/Interval Review"
     :is-provider-table="true"
   />
 </template>
@@ -41,12 +41,12 @@ export default {
         { key: 'lithCode2', label: 'Secondary Lithology Code' },
         { key: 'textCode1', label: 'Primary Texture Code' },
         { key: 'textCode2', label: 'Secondary Lithology Code' },
-        { key: 'compCode1', label: 'Lithologic Composition Code 1' },
-        { key: 'compCode2', label: 'Lithologic Composition Code 2' },
-        { key: 'compCode3', label: 'Lithologic Composition Code 3' },
-        { key: 'compCode4', label: 'Lithologic Composition Code 4' },
-        { key: 'compCode5', label: 'Lithologic Composition Code 5' },
-        { key: 'compCode6', label: 'Lithologic Composition Code 6' },
+        { key: 'compCode1', label: 'Other Component Code 1' },
+        { key: 'compCode2', label: 'Other Component Code 2' },
+        { key: 'compCode3', label: 'Other Component Code 3' },
+        { key: 'compCode4', label: 'Other Component Code 4' },
+        { key: 'compCode5', label: 'Other Component Code 5' },
+        { key: 'compCode6', label: 'Other Component Code 6' },
         { key: 'ageCodes', label: 'Geologic Ages' },
         { key: 'weight', label: 'Weight' },
         { key: 'rockLithCode', label: 'Rock Lithology Code' },
@@ -55,7 +55,7 @@ export default {
         { key: 'remarkCode', label: 'Remark Code' },
         { key: 'munsellCode', label: 'Munsell Code' },
         { key: 'exhausted', label: 'Exhausted' },
-        { key: 'intComments', label: 'Interval Comments' },
+        { key: 'intComments', label: 'Comments on Subsample/Interval' },
       ],
     };
   },
